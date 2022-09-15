f246f027bf408446a1180d60bbae06ac362c2b07
hamzabely1 <hamzaciao80@gmail.com>
2022-09-15 20:38:44 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  purge:[
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'plugins/**/*.js',
+    'nuxt.config.js'
   ],
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 }
