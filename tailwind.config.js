67d70658d50e84a43b3169a8609b88500a58265a
hamzabely1 <hamzaciao80@gmail.com>
2022-09-15 20:56:32 +0200

    coretion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  purge:[
-    'components/**/*.vue',
-    'layouts/**/*.vue',
-    'pages/**/*.vue',
-    'plugins/**/*.js',
-    'nuxt.config.js'
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
