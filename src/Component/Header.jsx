e1d4c33dedd3d6aac9eee29f2d7aa3d33c1f6a0d
hamzabely1 <hamzaciao80@gmail.com>
2022-09-15 13:16:27 +0200

    competence

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -16,33 +16,20 @@ function Presentation(props) {
                 class="items-center md:space-x-8 justify-center justify-items-start md:justify-items-center md:flex md:pt-2 w-full left-0 top-16 px-5 md:px-10 py-3 md:py-0 border-t md:border-t-0">
                 <a
                     class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300">
-                    Home
+                   Competénces
                 </a>
 
                 <a
                     class="flex text-gray-600 cursor-pointer transition-colors duration-300 font-semibold  hover:text-blue-500">
-                    Themes
+                    Portofolio
                 </a>
-
                 <a
-                    class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300">
-                    Developers
+                    class="flex text-gray-600 cursor-pointer transition-colors duration-300 font-semibold  hover:text-blue-500">
+                   Présentation
                 </a>
+              
 
-                <a
-                    class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300">
-                    Pricing
-                </a>
-
-                <a
-                    class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300">
-                    Blog
-                </a>
-
-                <a
-                    class="flex text-gray-600 hover:text-blue-500 cursor-pointer transition-colors duration-300">
-                    About Us
-                </a>
+               
             </div>
 
             <div class="flex items-center space-x-5 hidden md:flex">
